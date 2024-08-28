c74f61a8fd3ef9aa57dffe3250132c06f401c232
Dan Fincher <danfincher@microsoft.com>
2024-08-28 10:28:03 -0400

    initial setup for mf

--- src/HelloWorldStrings.resjson.ts
+++ src/HelloWorldStrings.resjson.ts
@@ -1,4 +1,4 @@
 export enum HelloWorldStrings {
-    HelloWorldMessage = "Hello World!",
+    HelloWorldMessage = "Hello World! This is from Module Federation Remote!",
     HelloWorldTitle = "Hello World Title",
 }
--- src/build.config.js
+++ src/build.config.js
@@ -1,5 +1,29 @@
+const { ModuleFederationPlugin } = require("webpack").container;
 const builder = require("@microsoft/azureportal-build").getExtensionBuilder();
 
 console.log("Building extension...");
 
-// builder.addPlugin
+builder.addPlugin(new ModuleFederationPlugin({
+    name: "remote",
+    filename: "remoteEntry.js",
+    exposes: {
+        "./HelloWorld": "./HelloWorld.ReactView",
+    },
+    shared: {
+        // Will probably need to expose our own module fedetraion plugin? Just to make sure that the shared modules are the same. Unless this doesnt matter since we serve up fluent, react, and react-dom..
+        // I should try to see what happens when I dont have these lines
+        react: {
+            singleton: true,
+        },
+        "react-dom": {
+            singleton: true,
+        },
+
+        /*
+            others:
+            
+            @microsoft/azureportal-reactview/
+            
+        */
+    }
+}));
