7633d0ebf6ee10b0379b36637700683941837a2b
Dan Fincher <danfincher@microsoft.com>
2024-10-09 11:29:14 -0400

    working

--- src/SharedComponent1.tsx
+++ src/SharedComponent1.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 
 export const SharedComponent1 = () => {
+
+    React.useEffect(() => {
+        console.log("I'm shared component1!");
+    }, []);
+
     return <div>Federated module component 1!</div>;
 }; 
--- src/SharedComponent2.tsx
+++ src/SharedComponent2.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 
 export const SharedComponent2 = () => {
+    
+    React.useEffect(() => {
+        console.log("I'm shared component2!");
+    }, []);
+
     return <div>Federated module component 2!</div>;
 }; 
